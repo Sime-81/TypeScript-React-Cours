0a15b3abf9a1bf27e27db8d747111976a6f091ca
sime-81 <yapock123@gmail.com>
2023-09-03 21:31:07 +0200

    TypeScript react course

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import { MessageInt } from "./model";
+import Message from "./components/Message";
 
-function App() {
+const App = () => {
+  const inputMessage = useRef<HTMLInputElement>(null);
+  const [messData, setMessData] = useState<MessageInt[]>([]);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (inputMessage) {
+      const mess: MessageInt = {
+        id: Math.round(Math.random() * Date.now()),
+        message: inputMessage?.current?.value,
+        date: Date.now(),
+      };
+      setMessData((prevData) => [...prevData, mess]);
+    }
+
+    (document.getElementById("inputMessage") as HTMLInputElement).value = "";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Poster ub message</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          placeholder="Entrez un message"
+          id="inputMessage"
+          ref={inputMessage}
+        />
+        <input type="submit" value="Envoyer" />
+      </form>
+      <h2>Liste des messages</h2>
+      <div>
+        {messData?.map((mess) => (
+          <Message
+            mess={mess}
+            messData={messData}
+            setMessData={setMessData}
+            key={mess.id}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
